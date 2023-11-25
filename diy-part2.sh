86fde47d06e421b91ae38acc7b502823112ec9f4
houchs <32591222+houchs@users.noreply.github.com>
2023-11-25 20:42:25 +0800

    Update diy-part2.sh +xiaozhuai / luci-app-filebrowser

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,6 +17,8 @@ rm -rf feeds/packages/net/smartdns
 rm -rf feeds/packages/net/mosdns
 
 #添加额外软件包
+git clone https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
+
 git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git lucky-repo
