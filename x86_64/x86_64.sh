4b21c98fecb785842644cc208ecbd883dbc34b80
houchs <32591222+houchs@users.noreply.github.com>
2023-12-01 18:05:35 +0800

    Update x86_64.sh 设置wan口为pppoe

--- x86_64/x86_64.sh
+++ x86_64/x86_64.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# 设置lan口ip为192.168.2.5
 sed -i 's/192.168.1.1/192.168.2.5/g' package/base-files/files/bin/config_generate
+# 设置wan口为pppoe
+sed -i 's/2:-dhcp/2:-pppoe/g' package/base-files/files/lib/functions/uci-defaults.sh
